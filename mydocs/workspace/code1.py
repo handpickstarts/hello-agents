b2a72667be52c060f7e6af0369853a9002c5087b
lei.jie <leijie@cidi.ai>
2025-12-26 15:56:33 +0800

    [fix]:[code1优化到可以执行getWeather]

--- mydocs/workspace/code1.py
+++ mydocs/workspace/code1.py
@@ -6,7 +6,7 @@ AGENT_SYSTEM_PROMT = """
 - getAdvice(city: str,weather: str) : 根据城市和天气，获取旅行的建议
 
 # 行动格式
-你的回答必须严格遵循以下格式。首先是你的思考过程，然后是你要执行的具体行动。每次回复只输出一对Thought-Action。
+你的回答必须要严格遵循以下格式。首先是你的思考过程，然后是你要执行的具体行动。每次回复只输出一对Thought-Action。
 Thought: [这里是你的思考过程和下一步计划]
 Action: [这里是你需要调用的工具，格式为 function_name(arg_name="arg_value")]
 
@@ -16,8 +16,6 @@ Action: [这里是你需要调用的工具，格式为 function_name(arg_name="a
 请开始吧!
 
 """
-
-from email import message
 from random import choice
 import requests
 import json
@@ -28,9 +26,9 @@ def getWeather(city: str) -> str:
     根据城市获取到城市天气,借助wttr.in API查询天气
     """
     # API断点，
-    url = f"http://wttr.in/{city}/?format=j1"
+    url = f"https://wttr.in/{city}?format=j1"
     try:
-     
+        print(f"-调用天气API,城市:{city}")
         response = requests.get(url)
         response.raise_for_status
         
@@ -51,6 +49,7 @@ def getAdvice(city : str,weather : str) -> str:
     """
     根据城市和天气，获取旅行的建议
     """
+    print(f"-调用Tavily API,城市:{city},天气:{weather}")    
     api_key = TAVILY_API_KEY # 推荐方式
     if not api_key:
         return "错误：未配置TAVILY_API_KEY环境变量"
@@ -116,7 +115,7 @@ import os
 
 # ollama
 LLM_API_KEY = None
-BASE_URL = "http://127.0.0.1:11434/v1"
+BASE_URL = "http://localhost:11434/v1"
 MODEL_ID = "deepseek-r1:7b"
 print(f"使用模型:{MODEL_ID}\n" + f"LLM_API_KEY:{LLM_API_KEY}\n" + f"BASE_URL:{BASE_URL}")
 
@@ -136,7 +135,7 @@ for i in range(1,5):
     print(f"-大模型回复:{llm_output}")
     # 输出会有多余的Thought-Action对，需要通过正则表示patter匹配截取，只取第一个
     th_ac_pattern = r'(Thought:.*?Action:.*?)(?=\n\s*(?:Thought:|Action:|Observation:)|\Z)'
-    match = re.search(pattern= th_ac_pattern, message = llm_output,type = re.DOTALL)
+    match = re.search(th_ac_pattern, llm_output, re.DOTALL)
     if match:
         truncated = match.group(1).strip()
         if truncated != llm_output.strip():
@@ -144,7 +143,7 @@ for i in range(1,5):
     else:
         print("-未匹配到有效Thought-Action对")
         break        
-    print("-截取到的有效回复:",llm_output)
+    print("-截取到的有效回复Thought-Action对:",llm_output)
     promt_history.append(llm_output)
     # 3.解析获取Action，并执行行动
     action_pattern = r'Action: (.*)'
@@ -154,27 +153,29 @@ for i in range(1,5):
         print("-模型未输出有效Action")
         break
     action_str = action_match.group(1).strip()
-    print("-解析到的Action:",action)
+    print("-解析到的Action: " + action_str)
 
     # 4 结束解析
     if action_str.startswith("finish"):
         finish_pattern = r'finish:\(answer:"(.*)"\)'
         finish_match = re.search(finish_pattern,llm_output,flags=re.DOTALL)
-        if finish_match:
-            finish = finish_match.group(1).strip()
-            print("-任务完成，最终答案为:",finish)
-            break
+        # finish = finish_match.group(1).strip()
+        print("-任务完成，最终答案为:",finish)
+        break
     
     # 5 执行行动 Action:(tool_name(arg_key="arg_value"))
-    tool_name = re.search(r'(\w+)\(',action_str,re.DOTALL)
-    args_str = re.search(r'\((.*?)\)',action_str,re.DOTALL)
-    kwargs = dict(re.findall(r'(\w+)="([^"]*)"',args_str,re.DOTALL))
+    tool_name = re.search(r'(\w+)\(',action_str,re.DOTALL).group(1)
+    args_str = re.search(r'\((.*?)\)',action_str,re.DOTALL).group(1)
+    kwargs = dict(re.findall(r'(\w+):"([^"]*)"',args_str))
+    print(f"-解析工具方法名:{tool_name},参数键值对:{kwargs}")
 
     if tool_name in available_tools:
+        print(f"-有可用工具{tool_name}")
+        tool_fun = available_tools[tool_name]
+        print(f"-执行工具方法 {tool_fun}")
         observation = available_tools[tool_name](**kwargs)
     else:
         observation =  f"-错误：未定义的工具 '{tool_name}'"
-
     observation_str = f"Observation: {observation}"
     print(f'{observation_str}\n' + "="*40)
     promt_history.append(observation_str)
